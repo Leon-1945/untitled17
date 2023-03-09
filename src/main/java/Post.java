a0632d2463ffbc5c3cddf610f709755af380fd80
Leon1945 <Muravlevevgeniy@yandex.ru>
2023-03-09 21:27:35 +0400

    Initial commit

---
+++ src/main/java/Post.java
@@ -0,0 +1,9 @@
+public class Post {
+    public String name;
+    public String passport;
+    public String petronymic;
+    public String phone;
+    public String surname;
+    public boolean subscriptio;
+    public FormDate birthday;
+}
